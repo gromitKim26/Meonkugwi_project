3cc5b6f5d3c3b190acb755b65fa93b4c24134e54
gromitKim26 <dudhkd12dbsk@gmail.com>
2025-05-24 19:11:36 +0900

    테스트 브랜치에 최종 병합 테스트

--- android-app/app/src/main/java/org/techtown/lovebike/OwnerFaceActivity.kt
+++ android-app/app/src/main/java/org/techtown/lovebike/OwnerFaceActivity.kt
@@ -20,6 +20,9 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.ByteArrayOutputStream
 import java.io.File
 import android.content.Intent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class OwnerFaceActivity : AppCompatActivity() {
     private lateinit var previewView: PreviewView
@@ -147,4 +150,17 @@ class OwnerFaceActivity : AppCompatActivity() {
     }
 
 
+}
+
+class OwnerFaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_owner_face)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
